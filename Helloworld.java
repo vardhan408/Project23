268665c90e22fb5019dabba4066a000b5ad2029f
vardhan408 <100558934+vardhan408@users.noreply.github.com>
2023-07-16 22:54:48 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,10 @@
-public class Sample2 {
+public class HelloWorld {
+
+    public static void printHelloWorld() {
+        System.out.println("Hello World!");
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        printHelloWorld();
     }
 }
